9c4d3631f2b4965d6f7e610e369270950147c0ad
Deleon <delps.santos1987@gmail.com>
2025-11-24 22:42:05 -0300

    ajudte funcional da tabela xml

--- xml_to_excel.py
+++ xml_to_excel.py
@@ -5,26 +5,26 @@ import json
 
 def xml_to_excel(arquivo_nome, valores):
     
-    with open(f'xml/{arquivo_nome}', 'rb', encoding='utf-8') as arquivo_xml:
+    with open(f'xml/{arquivo_nome}', 'rb') as arquivo_xml:
         json_arquivo = xmltodict.parse(arquivo_xml)
         # Convert the ordered dictionary to a JSON string
         json_string = json.dumps(json_arquivo, indent=4)
-        print(json_string)
+        #print(json_string)
 
         infor_nfe = json_arquivo['nfeProc']['NFe']['infNFe'] if 'nfeProc' in json_arquivo else json_arquivo['NFe']['infNFe']
         dados_nfe = infor_nfe['@Id'] if '@Id' in infor_nfe else 'nao informado'
-        dados_emitente = infor_nfe['emit'] if 'emit' in infor_nfe else 'nao informado'  
-        dados_destinatario = infor_nfe['dest'] if 'dest' in infor_nfe else 'nao informado'
-        dados_produtos = infor_nfe['det'] if 'det' in infor_nfe else 'nao informado'
-        dados_totais = infor_nfe['total']['ICMSTot'] if 'total' in infor_nfe and 'ICMSTot' in infor_nfe['total'] else 'nao informado'
-        dados_transporte = infor_nfe['transp'] if 'transp' in infor_nfe else 'nao informado'
+        dados_emitente = infor_nfe['emit']['xNome'] if 'emit' in infor_nfe else 'nao informado'  
+        dados_destinatario = infor_nfe['dest']['xNome'] if 'dest' in infor_nfe else 'nao informado'
+        dados_produtos = infor_nfe['det']['prod']['xProd'] if 'det' in infor_nfe else 'nao informado'
+        dados_totais = infor_nfe['det']['prod']['vProd']if 'det' in infor_nfe and 'ICMSTot' in infor_nfe['total'] else 'nao informado'
+        dados_transporte = infor_nfe['pag']['detPag']['vPag'] if 'pag' in infor_nfe else 'nao informado'
         valores.append([dados_nfe, dados_emitente, dados_destinatario, dados_produtos, dados_totais, dados_transporte])
 
 
     
     
 arquivo  = os.listdir('xml')
-colunas = ['n°_nfe', 'emitente', 'destinatario', 'produtos', 'totais', 'transporte']
+colunas = ['n°_nfe', 'emitente', 'destinatario', 'produtos', 'total_item', 'valor_total_nfe']
 valores = []
 
 for file in arquivo:
@@ -33,3 +33,4 @@ for file in arquivo:
 
 
 tabela_excel = pd.DataFrame(valores, columns=colunas)
+tabela_excel.to_excel('tabela_nfe.xlsx', index=False)
